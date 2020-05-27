eb6efd205ae33fa997c6be473a223c14fbcfb4e7
Safia Hirsi <safa.hirsi@gmail.com>
2020-05-26 22:46:59 -0500

    update recipe-list html

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'Test Recipe description',
+      'https://img.buzzfeed.com/thumbnailer-prod-us-east-1/video-api/assets/216054.jpg'
+    ),
+  ];
   constructor() {}
 
   ngOnInit(): void {}
